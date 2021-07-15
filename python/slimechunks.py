0ba55ed1a8e7e740408f94ea92990d31e393edca
Pfannekuchen <gamesupport@mgnet.work>
2021-07-15 19:39:50 +0200

    Add Python

---
+++ python/slimechunks.py
@@ -0,0 +1,23 @@
+# from numba import njit, vectorize, int64
+from numba import njit
+# import numpy as np
+import time
+
+@njit
+def seed(worldseed: int, xPosition: int, zPosition: int):
+    return (((((worldseed + (xPosition * xPosition * 0x4c1906) + (xPosition * 0x5ac0db) + (zPosition * zPosition) * 0x4307a7 + (zPosition * 0x5f24f) ^ 0x3ad8025f) ^ 0x5DEECE66D) & 0xffffffffffff) * 0x5DEECE66D + 0xB & 0xffffffffffff) >> 17) % 10
+
+@njit
+def save(f: int):
+    num = 0
+    for i in range(f):
+        num += 1 if seed(i, 420, 69) == 0 else 0
+    return num
+
+save(1)
+
+start = time.time()
+r = save(1000000000)
+end = time.time()
+print(r)
+print(round(end - start, 2))
