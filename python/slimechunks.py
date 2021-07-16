6a1fefe6ea6142e0d6fef3916953f37329a95288
BlockOG <68442822+BlockOG@users.noreply.github.com>
2021-07-16 15:41:26 +0300

    Optimized python a bit

--- python/slimechunks.py
+++ python/slimechunks.py
@@ -5,11 +5,12 @@ from os import cpu_count
 def seed(worldseed: int, seedpart: int):
     return ((((((worldseed + seedpart) ^ 0x3ad8025f) ^ 0x5DEECE66D) & 0xffffffffffff) * 0x5DEECE66D + 0xB & 0xffffffffffff) >> 17) % 10
 
+xPosition = 420
+zPosition = 69
+seedpart = (xPosition * xPosition * 0x4c1906) + (xPosition * 0x5ac0db) + (zPosition * zPosition) * 0x4307a7 + (zPosition * 0x5f24f)
+
 def save(g: int, f: int):
-    num = 0
-    xPosition = 420;
-    zPosition = 69;
-    seedpart = (xPosition * xPosition * 0x4c1906) + (xPosition * 0x5ac0db) + (zPosition * zPosition) * 0x4307a7 + (zPosition * 0x5f24f);
+    num = 0    
     for i in range(g, f):
         num += 1 if seed(i, seedpart) == 0 else 0
     return f - g, num
@@ -33,5 +34,5 @@ for i in result:
 
 print("Seeds found:", r[1])
 print("Seeds checked:", r[0])
-print("Seconds ran:", end - start)
+print("Seconds ran:", round(nd - start, 2))
 print(round(r[0]/(end-start)), "seeds per second")
